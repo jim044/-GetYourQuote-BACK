7702de5d68d727ec256833678b77840b5582b1b0
jim044 <jimmy.villossel@gmail.com>
2020-05-25 13:53:08 +0200

    Second commit

---
+++ src/test/java/com/kode4you/getyourquote/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kode4you.getyourquote.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
